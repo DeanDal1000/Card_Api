726b862e1735958f34d5d7bbc45eb35799e943ff
DeanDal1000 <dalzelldean@gmail.com>
2021-01-06 12:03:23 +0000

    API Display Name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
 
-function App() {
+function App(){
+  const [characters, setCharacters ] =  useState(null);
+
+  const apiURL = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
+  
+  const fetchData = async () => {
+    const response = await axios.get(apiURL)
+    
+    setCharacters(response.data)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-      Hello
-      </header>
-    </div>
-  );
-}
+      <h1> Superheros </h1>
+      <h2>Fetch a list from an API and Display it</h2>
 
+    {/* // Fetch Data */}
+    <div>
+      <button className="fetch-button" onClick={fetchData}>
+        Fetch Data
+      </button>
+    </div>
+
+    <div className="characters">
+      {characters &&
+        characters.map((character, index) => {
+          
+        return (
+          <div className="character" key={index}>
+          <h3>Character {index + 1}</h3>
+          <h2>{character.name}</h2>
+
+          <div className="details">
+          </div>
+        </div>
+        )
+        })}
+    </div>
+
+    </div>
+  )
+}
+ 
 export default App;
